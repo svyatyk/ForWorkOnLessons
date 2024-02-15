c1553786cbc9f91ec3f443bec96c2b2bb7576c48
svyatyk <150888677+svyatyk@users.noreply.github.com>
2024-02-15 20:33:18 +0200

    firstLessons

---
+++ script.js
@@ -0,0 +1,40 @@
+// let text = ";alskjdfkasjdfjaklsjd;fja;sljdf;ajsdjfl;asdjflaskjdfl;kasjblvj nclxjzg.jdf,f";
+// const numbers = 123424343;
+alert("text");
+let text = "sldjfa;ldj;lafs" //"" - type of data = text
+window.alert("our first js lesson")
+let Number = 4;
+typeof(Number);
+console.log(typeof(Number));
+let ddd;
+console.log(typeof("und"));
+console.log(typeof(ddd));
+const woman = {
+    name: 'maria',
+    age : 12,
+    isMarried :true
+
+
+};
+console.log(woman);
+woman.age= 4;
+
+console.log(woman.age);
+
+let array = ["adsdf" , 1, true ];
+console.log(arr[1]);
+
+let NameStore = lidl;
+const StoreDescription = {
+     budget : 10000,
+     employees : ["sss", "ddd", "fff"],
+     products :  {
+        trousers : 100,
+        hhh : 100
+
+     },
+     shopIsOpen : true,
+     shopIsClosed: false
+}
+let name;
+name = "sdf";
